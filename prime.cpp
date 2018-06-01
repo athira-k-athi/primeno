966986ab8d36c3c08077633e620fda4468138420
athi <athirak7919@gmail.com>
2018-06-01 10:40:05 +0530

    first version of prime

---
+++ prime.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+int main()
+{
+    int value;
+    read:
+    bool is_prime = true;
+    std::cout<<"enter a number : ";
+    std::cin>>value;
+    if(value==1)
+    {
+        std::cout<<"1 is neither prime nor composite"<<'\n';
+    }
+    else if(value<0)
+    {
+        std::cout<<"entered a -ve number"<<'\n';
+    }
+    else
+    {
+        for(int trial = 2;(is_prime)&&(trial<value); trial++)
+        {
+            is_prime = (value%trial!=0);
+        }
+        if(is_prime==1)
+                std::cout<<value<<" is a prime number"<<'\n';
+        else
+        std::cout<<value<<" is not a prime number"<<'\n';
+    }
+    goto read;
+
+}
+
